a4607d58dfc5722d3e534ddc6d93ed5c199c6d77
VASUDHA K T <VASUDHA K T@vasudha>
2024-08-27 21:39:18 +0530

    Safe commit

---
+++ Tnsif2025/src/com/tnsif/day1/DesitionMakingWithOperator.java
@@ -0,0 +1,27 @@
+package com.tnsif.day1;
+
+public class DesitionMakingWithOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=8,y=7;
+		int a=10,b=11;
+		if(x<=y)
+		{
+			System.out.println("true");
+		}
+		else {
+			System.out.println("false");
+		}
+		if(!(a<b)&&(a==b)) {//false&&false
+			System.out.println("true");
+		}
+		else {
+			System.out.println("false");
+		}
+			
+		}
+
+	}
+
+
